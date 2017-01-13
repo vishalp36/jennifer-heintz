b902eda2abf9385ea22fa3d2fdf9179f2392cd79
mikehwagz <hmiklw@me.com>
2017-01-12 22:46:06 -0500

    create about page with 3d mousemove animation

--- assets/js/sections/about.js
+++ assets/js/sections/about.js
@@ -1,6 +1,7 @@
 import config from 'config'
 import utils from 'utils'
 import classes from 'dom-classes'
+import { on, off } from 'dom-event'
 import Default from './default'
 
 class About extends Default {
@@ -10,6 +11,12 @@ class About extends Default {
 		super(opt)
 
 		this.slug = 'about'
+		
+		this.page = { x: 0, y: 0 }
+		this.ease = { x: 0, y: 0 }
+		
+		this.onMouseMove = this.onMouseMove.bind(this)
+		this.run = this.run.bind(this)
 	}
 	
 	init(req, done) {
@@ -20,8 +27,40 @@ class About extends Default {
 	ready(done) {
 		
 		super.ready()
+		
+		this.addEvents()
 
 		done()
+	}
+	
+	addEvents() {
+		
+		on(config.body, 'mousemove', this.onMouseMove)
+	}
+	
+	removeEvents() {
+		
+		off(config.body, 'mousemove', this.onMouseMove)
+		
+		cancelAnimationFrame(this.run)
+	}
+	
+	onMouseMove(evt) {
+		
+		this.page.x = (evt.pageX - config.width / 2) / config.width * 6
+  	this.page.y = (evt.pageY - config.height / 2) / config.height * 6
+		
+		requestAnimationFrame(this.run)
+	}
+	
+	run() {
+		
+		this.ease.x += (this.page.x - this.ease.x) * .05
+		this.ease.y += (this.page.y - this.ease.y) * .05
+		
+		this.ui.bio.style.transform = `rotateX(${this.ease.y}deg) rotateY(${-this.ease.x}deg)`
+		
+		requestAnimationFrame(this.run)
 	}
 
 	animateIn(req, done) {
@@ -49,6 +88,8 @@ class About extends Default {
 	destroy(req, done) {
 
 		super.destroy()
+		
+		this.removeEvents()
 
 		this.page.parentNode.removeChild(this.page)
 		

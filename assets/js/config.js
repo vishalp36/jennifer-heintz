76df07c8f8459ed0b908fa2974f604846e5ecfb4
mikehwagz <hmiklw@me.com>
2017-03-04 23:39:50 -0500

    add light ui logic, some animation tuning

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,13 +1,13 @@
 import domselect from 'dom-select'
 
 const config = {
-	
+
 	BASE: '/',
-	
+
 	body: document.body,
 	view: domselect('main'),
 	nav: domselect.all('.js-nav'),
-    
+
 	width: window.innerWidth,
 	height: window.innerHeight
 }
--- assets/js/sections/single.js
+++ assets/js/sections/single.js
@@ -11,10 +11,17 @@ class Single extends Default {
 
 		super(opt)
 
+		this.bindEvents()
+
 		this.slug = 'single'
 
 		this.isProjectNavClick = false
-		this.onProjectNavClick = this.onProjectNavClick.bind(this)
+	}
+
+	bindEvents() {
+
+		['onSlide', 'onProjectNavClick']
+			.forEach(fn => this[fn] = this[fn].bind(this))
 	}
 
 	init(req, done) {
@@ -39,56 +46,6 @@ class Single extends Default {
 
 	initSlides() {
 
-		this.setSlides()
-
-		this.slider = new Manager({
-		  length: this.slides.length - 1,
-		  callback: (evt) => {
-
-	      const index = evt.current
-	      const previous = evt.previous
-	      const down = evt.direction === 'downwards'
-
-	      this.slider.animating = true
-
-				const currentVideo = this.slides[index].querySelector('video') || null
-				const previousVideo = this.slides[previous].querySelector('video') || null
-
-				if (currentVideo) {
-					currentVideo.play()
-				}
-
-				if (previousVideo) {
-					previousVideo.pause()
-				}
-
-	      const tl = new TimelineMax({ paused: true, onStart: _ => {
-					index === 0 ? this.setNavColor('light') : this.setNavColor()
-				}, onComplete: _ => {
-	        setTimeout(_ => {
-						this.slider.animating = false
-					}, 200)
-	      }})
-
-	      tl.staggerTo(this.slides, .9, { cycle: {
-	        y: (loop) => index === loop ? 0 : loop < index ? -config.height : config.height,
-					zIndex: (loop) => index === loop ? 2 : 1
-	      }, ease: Power3.easeInOut}, 0, 0)
-
-	      tl.restart()
-		  }
-		})
-
-		this.slider.init()
-	}
-
-	setSlides() {
-		const heroBlock = this.slides[0].children[0]
-
-		if (heroBlock.hasAttribute('data-light-ui')) {
-			this.setNavColor('light')
-		}
-
 		this.slides.forEach(slide => {
 
 			const index = this.slides.indexOf(slide)
@@ -96,15 +53,60 @@ class Single extends Default {
 
 			slide.style.transform = `translateY(${height * index}px)`
 		})
+
+		this.slider = new Manager({
+		  length: this.slides.length - 1,
+		  callback: this.onSlide
+		})
+
+		this.slider.init()
 	}
 
-	setNavColor(c) {
+	setNavColor(index) {
 
-		const color = c || 'dark'
+		const color = this.slides[index].querySelector('.block').dataset.light === 'true' ? '#F9F9F9' : '#2B2B2B'
 
 		config.nav.forEach(el => {
-			el.style.color = color === 'light' ? '#F9F9F9' : '#2b2b2b'
+			el.style.color = color
 		})
+	}
+
+	onSlide(evt) {
+
+		const index = evt.current
+		const previous = evt.previous
+		const down = evt.direction === 'downwards'
+
+		this.slider.animating = true
+
+		const currentVideo = this.slides[index].querySelector('video') || null
+		const previousVideo = this.slides[previous].querySelector('video') || null
+
+		if (currentVideo) {
+			currentVideo.play()
+		}
+
+		if (previousVideo) {
+			previousVideo.pause()
+		}
+
+		const tl = new TimelineMax({ paused: true,
+			onStart: _ => {
+				this.setNavColor(index)
+			},
+			onComplete: _ => {
+				setTimeout(_ => {
+					this.slider.animating = false
+				}, 200)
+			}
+		})
+
+		tl.staggerTo(this.slides, .9, { cycle: {
+			y: (loop) => index === loop ? 0 : loop < index ? -config.height : config.height,
+			zIndex: (loop) => index === loop ? 2 : 1
+		}, ease: Expo.easeInOut}, 0, 0)
+
+		tl.restart()
 	}
 
 	onProjectNavClick(evt) {
@@ -117,11 +119,11 @@ class Single extends Default {
 	animateIn(req, done) {
 
 		classes.add(config.body, `is-${this.slug}`)
+
 		const canvas = document.querySelector('canvas')
-		const tl = new TimelineMax({paused: true, onComplete: _ =>{
+		const tl = new TimelineMax({ paused: true, onComplete: _ => {
 
 			done()
-
 			// remove canvas
 			req.previous && req.previous.route === '/' && canvas.parentNode.removeChild(canvas)
 
@@ -140,6 +142,9 @@ class Single extends Default {
 				ease: Expo.easeInOut,
 				delay: 1.6
 			})
+			tl.add(_ => {
+				this.setNavColor(0)
+			})
 			tl.to(canvas, .6, {
 				autoAlpha: 0,
 				ease: Expo.easeInOut
@@ -149,10 +154,10 @@ class Single extends Default {
 		} else {
 
 			// if not, just fade in the page
-			tl.to(this.page, 1, {
-				autoAlpha: 1,
-				ease: Expo.easeInOut
+			tl.add(_ => {
+				this.setNavColor(0)
 			})
+			tl.set(this.page, { delay: 1, autoAlpha: 1 })
 			tl.restart()
 		}
 	}
@@ -161,17 +166,22 @@ class Single extends Default {
 
 		classes.remove(config.body, `is-${this.slug}`)
 
-		const tl = new TimelineMax({ paused: true, onComplete: done })
+		const tl = new TimelineMax({ paused: true, onStart: _ => {
+
+			config.nav.forEach(el => {
+				el.style.color = '#2B2B2B'
+			})
+
+		}, onComplete: done })
 
 		if (req.params.id && this.isProjectNavClick) {
 
-			const mask = this.ui[`${this.direction}ProjectMask`]
-			const inner = this.ui[`${this.direction}Project`]
+			const img = this.ui[`${this.direction}Project`]
 
-			tl.set(mask, {
+			tl.set(img, {
 				zIndex: 20
 			})
-			tl.to([mask, inner], 1, {
+			tl.to(img, 1, {
 				x: 0,
 				ease: Expo.easeInOut
 			})

5c44c8b23d2dbb4b9f29c37b0de13274a5648d97
mikehwagz <hmiklw@me.com>
2017-03-18 23:35:03 -0400

    remove animated border from preloader

--- assets/js/sections/preloader.js
+++ assets/js/sections/preloader.js
@@ -49,12 +49,7 @@ class Preloader {
 						<h2 class="preloader__subtitle">Designer & Illustrator</h2>
 					</div>
 				</div>
-				<div class="preloader__border preloader__border--top preloader__border--horizontal js-top"></div>
-				<div class="preloader__border preloader__border--right preloader__border--vertical js-right"></div>
-				<div class="preloader__border preloader__border--bottom preloader__border--horizontal js-bottom"></div>
-				<div class="preloader__border preloader__border--left preloader__border--vertical js-left"></div>
-			</div>
-			`
+			</div>`
 		})
 
 		this.view.insertBefore(this.el, page)
@@ -68,22 +63,14 @@ class Preloader {
 
 	animateIn(req, done) {
 
-		const preloaderBorders = document.querySelectorAll('.preloader__border')
-		const appBorder = document.querySelector('.js-border')
 		const text = document.querySelector('.preloader__text')
 
 		const tl = new TimelineMax({ paused: true, onComplete: () => {
 			done()
 			this.preloaded()
 		}})
-
-		tl.to(text, 1.75, {autoAlpha: 1, ease: Linear.easeNone}, 'start')
-		tl.staggerTo(preloaderBorders, .4, {
-			cycle: {
-				transform: (i) => i === 0 || i === 2 ? 'scaleX(1)' : 'scaleY(1)'
-			}
-		}, 0.3, 'start')
-		tl.set(appBorder, { opacity: 1 })
+		
+		tl.to(text, 1.75, { autoAlpha: 1, ease: Linear.easeNone })
 		tl.restart()
 	}
 

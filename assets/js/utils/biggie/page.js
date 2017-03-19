c03df16f008b2e732c3616ee63247f89b1c9097a
mikehwagz <hmiklw@me.com>
2017-03-18 21:37:41 -0400

    get adjacent project data

--- assets/js/utils/biggie/page.js
+++ assets/js/utils/biggie/page.js
@@ -23,6 +23,28 @@ export default (req, view, options, done) => {
       const blocks = ['image', 'split', 'text', 'video']
       const templates = ['single', ...blocks.map(block => `components/${block}`)]
       const urls = templates.map(tmpl => `${config.BASE}templates/${tmpl}.mst`)
+      const keys = Object.keys(projects)
+      const current = keys.indexOf(req.params.id)
+      const prev = current === 0 ? keys.length -1 : current - 1
+      const next = current === keys.length - 1 ? 0 : current + 1
+      const nextProject = projects[keys[next]]
+      const prevProject = projects[keys[prev]]
+      const nextHeroBlock = nextProject.content[0]
+      const nextHeroImgSrc = nextHeroBlock.image ? nextHeroBlock.image.src : nextHeroBlock.video.still
+      const prevHeroBlock = prevProject.content[0]
+      const prevHeroImgSrc = prevHeroBlock.image ? prevHeroBlock.image.src : prevHeroBlock.video.still
+
+      data['next'] = {
+        title: nextProject.title,
+        slug: nextProject.slug,
+        hero: nextHeroImgSrc
+      }
+
+      data['prev'] = {
+        title: prevProject.title,
+        slug: prevProject.slug,
+        hero: prevHeroImgSrc
+      }
 
       Promise.all(
         urls.map(url => fetch(url).then(res => res.text()))
@@ -49,8 +71,8 @@ export default (req, view, options, done) => {
 
       fetch(`${config.BASE}templates/${id}.mst`)
         .then(res => res.text())
-        .then(data => {
-          const rendered = Mustache.render(object.data, data)
+        .then(tmpl => {
+          const rendered = Mustache.render(tmpl, data)
           page.innerHTML = rendered
           cache[id] = rendered
           done()
